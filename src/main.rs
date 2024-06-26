52108fe09f6e21da0d9e4fee9b82b6f39cea4b54
Shivaji Chalise <respond2shivaji@gmail.com>
2024-06-26 16:01:16 +0545

    hello world!!!
    - htmx + barebone rust

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,35 @@
 use std::{
-    io::Read,
+    fs,
+    io::{Read, Write},
     net::{SocketAddr, TcpListener, TcpStream},
     result,
 };
 
 const IP: &str = "127.0.0.1";
 const PORT: u16 = 6969;
+const OK_STATUS: &str = "HTTP/1.1 200 OK";
+const NOT_FOUND_STATUS: &str = "HTTP/1.1 200 OK";
 
 type Result<T> = result::Result<T, ()>;
+
+fn respond(mut stream: TcpStream, status_line: String, content: String) -> Result<()> {
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        content.len(),
+        content
+    );
+
+    stream.write(response.as_bytes()).map_err(|err| {
+        eprintln!("ERROR: Cannot send response: {err}");
+    })?;
+
+    stream.flush().map_err(|err| {
+        eprintln!("ERROR: Failed to flush the stream: {err}");
+    })?;
+
+    Ok(())
+}
 
 fn handle_client(mut stream: TcpStream) -> Result<()> {
     let mut buffer = [0; 1024];
@@ -16,8 +38,19 @@ fn handle_client(mut stream: TcpStream) -> Result<()> {
         eprintln!("ERROR: Cannot read the connection stream from client: {err}");
     })?;
 
-    println!("INFO: Read connection into buffer");
-    println!("{}", String::from_utf8_lossy(&buffer[..]));
+    let home = b"GET / HTTP/1.1\r\n";
+    let test = b"GET /test HTTP/1.1\r\n";
+
+    if buffer.starts_with(home) {
+        let index_page = fs::read_to_string("index.html").unwrap();
+        respond(stream, OK_STATUS.to_string(), index_page)?;
+    } else if buffer.starts_with(test) {
+        let message = String::from("HELLO WORLD");
+        respond(stream, OK_STATUS.to_string(), message)?;
+    } else {
+        let not_found = fs::read_to_string("404.html").unwrap();
+        respond(stream, NOT_FOUND_STATUS.to_string(), not_found)?;
+    }
 
     Ok(())
 }
